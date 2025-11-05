845bfa1707f48afcc2b6bb1440c27ce2ec6ddb38
Mattes Gärtner <mattesgaertner@MacBook-Pro-von-Mattes.local>
2025-11-05 11:32:19 +0100

    regs implemented

--- CPU/rtl/regs/regs.sv
+++ CPU/rtl/regs/regs.sv
@@ -13,4 +13,26 @@ module regs (
     output logic [31:0] rs2
 );
 
+    reg [31:0] R[31:0];
+
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            rs1 <= 32'h00000000;
+            rs2 <= 32'h00000000;
+        end
+        else if (regwrite) begin
+            R[rdadr] <= rd;
+        end
+        else begin
+            case (rs1adr)
+                5'b00000    :   rs1 <= 32'h00000000;
+                default     :   rs1 <= R[rs1adr];
+            endcase
+            case (rs2adr)
+                5'b00000    :   rs2 <= 32'h00000000;
+                default     :   rs2 <= R[rs2adr];
+            endcase
+        end
+    end
+
 endmodule  // regs
